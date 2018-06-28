56fc85692463e7cfaa9c83e6d5b70d6a225e9ed3
Tom <t.oliver.ball@protonmail.ch>
2018-06-28 17:38:22 +0100

    Javadoced the OptionallySized interface

--- src/main/java/xawd/jflow/iterators/OptionallySized.java
+++ src/main/java/xawd/jflow/iterators/OptionallySized.java
@@ -4,11 +4,19 @@
 package xawd.jflow.iterators;
 
 /**
+ * Abstraction of the concept of a sequence having an 'Optional size', more
+ * accurately it describes a sequence which has a (possibly infinite) size but
+ * the specific quantity may not be known.
+ *
  * @author ThomasB
  */
 @FunctionalInterface
 public interface OptionallySized
 {
+	/**
+	 * @return The size of the sequence if it known, otherwise a negative number to
+	 *         indicate the value is unknown.
+	 */
 	int size();
 
 	default boolean isSized()
