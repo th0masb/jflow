38ac6a2e7446e1a6a075f7a32864dce2d4a71ba4
Tom <tomoliverball@gmail.com>
2019-06-09 18:54:09 +0100

    Updated examples for presentation

--- src/examples/java/com/github/maumay/jflow/exp/Presentation.java
+++ src/examples/java/com/github/maumay/jflow/exp/Presentation.java
@@ -5,11 +5,14 @@ package com.github.maumay.jflow.exp;
 
 import static com.github.maumay.jflow.vec.Vec.vec;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import com.github.maumay.jflow.iterators.Iter;
 import com.github.maumay.jflow.vec.Vec;
@@ -62,6 +65,34 @@ public class Presentation
 
 		Vec<String> result = Iter.over(left).zip(right.iter())
 				.map(pair -> doSomething(pair._1, pair._2)).toVec();
+
+		Vec<Square> squares = getSomeSquares();
+		Vec<Circle> circles = getSomeCircles();
+		Vec<Triangle> triangles = getSomeTriangles();
+
+		Vec<Shape> shapes = squares.iter().<Shape>cast()
+				.append(circles.iter(), triangles.iter()).toVec();
+
+		Vec<String> strings = vec("a", "b", "c");
+
+		List<String> list = strings.toList();
+		Set<String> set = strings.toSet();
+		Deque<String> deque = strings.to(ArrayDeque::new);
+	}
+
+	private static Vec<Triangle> getSomeTriangles()
+	{
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	private static Vec<Circle> getSomeCircles()
+	{
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	private static Vec<Square> getSomeSquares()
+	{
+		throw new RuntimeException("Not yet implemented");
 	}
 
 	public static String doSomething(String left, String right)
@@ -72,5 +103,21 @@ public class Presentation
 	private static Vec<Iterator<String>> getSomeIterators()
 	{
 		throw new RuntimeException();
+	}
+
+	interface Shape
+	{
+	}
+
+	class Square implements Shape
+	{
+	}
+
+	class Circle implements Shape
+	{
+	}
+
+	class Triangle implements Shape
+	{
 	}
 }
