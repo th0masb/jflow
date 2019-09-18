36e5d4a0faa49478f05397fd66f111e0cca93d30
Steven Canipa <steven.canipa@lhasalimited.org>
2019-09-18 09:56:05 +0100

    Add module descriptor encapsulating single implementation package

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module jflow.iterator {
+    
+    exports com.github.maumay.jflow.iterables;
+    exports com.github.maumay.jflow.iterators;
+    exports com.github.maumay.jflow.utils;
+    exports com.github.maumay.jflow.vec;
+}
